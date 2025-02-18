65f6be0b03ed76bc85574bb6114df955170f592b
Jesus_Silva <jesus.silvap@gmail.com>
2025-02-18 09:54:12 -0500

    Ejercici #1 #2

---
+++ script.js
@@ -0,0 +1,17 @@
+//Ejercicio 1
+
+let nombre = "Jesus Silva";
+let edad = 35;
+let ciudad = "Garzón Huila";
+console.log("Mi nombre es " + nombre + ", tengo " + edad + " años y vivo en " + ciudad);
+
+/* Crea una variable llamada precio y asígnale un valor decimal. 
+Luego, crea otra variable llamada cantidad y asígnale un valor entero. 
+Muestra el tipo de dato de cada variable en la consola.*/
+
+let precio = 3.14;
+let cantidad = 5;
+
+console.log(typeof precio); // Imprime en la consola
+document.write(typeof cantidad); // Imprime en el navegador
+alert(typeof cantidad); // Imprime en una ventana emergente
